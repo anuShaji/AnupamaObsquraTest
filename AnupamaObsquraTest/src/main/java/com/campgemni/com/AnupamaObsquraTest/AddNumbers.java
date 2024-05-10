ba2bb62ca7874c4153013df352ad947bd794da89
ANUPAMA VISHNU <ANUPAMA VISHNU@LAPTOP-QEKB9P62>
2024-05-10 13:57:32 +0400

    Anupama is testing commit

---
+++ AnupamaObsquraTest/src/main/java/com/campgemni/com/AnupamaObsquraTest/AddNumbers.java
@@ -0,0 +1,19 @@
+package com.campgemni.com.AnupamaObsquraTest;
+
+
+	
+public class AddNumbers {
+	public void add() {
+		int a=10,b=50;
+		int sum=a+b;
+		System.out.println("sum is:"+sum);
+	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	AddNumbers ad=new AddNumbers();
+	ad.add();
+		}
+
+	}
+
+
